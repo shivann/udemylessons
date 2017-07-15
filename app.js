d2f6acecb7c662fb4894af1aa4a8e6a29c22d985
Shivan.Naidoo <Shivan.Naidoo@interfront.co.za>
2017-07-15 18:54:12 +0200

    Customer Service

--- app.js
+++ app.js
@@ -1,2 +1,50 @@
 // MODULE
 var weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
+
+//ROUTES
+
+weatherApp.config(function($routeProvider) {
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+    })
+
+    .when('/forecast', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+
+});
+
+//SERVICES
+
+weatherApp.service('cityService', function() {
+
+    this.city = "New York, NY";
+
+});
+
+
+//CONTROLLERS
+
+
+//home.htm
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city;
+    });
+
+}]);
+
+//forecast.htm
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+
+    $scope.city = cityService.city;
+
+}]);
